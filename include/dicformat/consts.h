675eed0cfb9991e1b940eba4aad4e1e0a34b6dac
Natalia Portillo <claunia@claunia.com>
2019-03-16 17:51:33 +0000

    Add constants.

--- include/dicformat/consts.h
+++ include/dicformat/consts.h
@@ -33,4 +33,25 @@
 #ifndef LIBDICFORMAT_CONSTS_H
 #define LIBDICFORMAT_CONSTS_H
 
+/** Magic identidier = "DICMFMT". */
+#define DIC_MAGIC = 0x544D52464D434944;
+/** Image format version. A change in this number indicates an incompatible change to the format that prevents older implementations from reading it correctly, if at all. */
+#define DICF_VERSION = 1;
+/** Maximum read cache size, 256MiB. */
+#define MAX_CACHE_SIZE = 256 * 1024 * 1024;
+/** Size in bytes of LZMA properties. */
+#define LZMA_PROPERTIES_LENGTH = 5;
+/** Maximum number of entries for the DDT cache. */
+#define MAX_DDT_ENTRY_CACHE = 16000000;
+/** How many samples are contained in a RedBook sector. */
+#define SAMPLES_PER_SECTOR = 588;
+/** Maximum number of samples for a FLAC block. Bigger than 4608 gives no benefit. */
+#define MAX_FLAKE_BLOCK = 4608;
+/** Minimum number of samples for a FLAC block. CUETools.Codecs.FLAKE does not support it to be smaller than 256. */
+#define MIN_FLAKE_BLOCK = 256;
+/** This mask is to check for flags in CompactDisc suffix/prefix DDT */
+#define CD_XFIX_MASK = 0xFF000000;
+/** This mask is to check for position in CompactDisc suffix/prefix deduplicated block */
+#define CD_DFIX_MASK = 0x00FFFFFF;
+
 #endif //LIBDICFORMAT_CONSTS_H
